17436f1edeae060ac18ffae23d15f313eb6864e0
jhuanassumpcao <jhuanassumpcao@gmail.com>
2023-04-12 15:31:51 -0300

    add ItemCompra

--- Loja.java
+++ Loja.java
@@ -102,3 +102,26 @@ public class CarrinhoDeCompras {
         return null;
     }
 }
+
+public class ItemCompra {
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemCompra(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
